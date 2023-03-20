bd5c5b4524405147ed7b0e1287515ed67f6fd5cf
VishakhaRapelli <125191036+VishakhaRapelli@users.noreply.github.com>
2023-03-20 13:49:35 +0530

    Add files via upload

---
+++ Posneg.java
@@ -0,0 +1,14 @@
+class Posneg{
+public static void main(String args[])
+{
+int n=20;
+if(n>0)
+{
+System.out.println("number is positive ");
+}
+else
+{
+System.out.println("number is negative");
+}
+}
+}
