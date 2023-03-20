9e4cf746f0e8fc7ef800e502e8ae9835e42c3a69
VishakhaRapelli <125191036+VishakhaRapelli@users.noreply.github.com>
2023-03-20 13:48:17 +0530

    Add files via upload

---
+++ Leapyear.java
@@ -0,0 +1,14 @@
+class Leapyear{
+public static void main(String args[])
+{
+int year=2022;
+if(year%4==0)
+{
+System.out.println("leap year");
+}
+else
+{
+System.out.println("not leap year");
+}
+}
+}
---
+++ Multiplication.java
@@ -0,0 +1,9 @@
+class Multiplication{
+public static void main(String args[]){
+int num =2;
+for( int i=1;i<=10;i++)
+{
+System.out.println(i*num);
+}
+}
+} 
