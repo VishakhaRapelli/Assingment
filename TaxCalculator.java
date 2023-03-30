2a0a6e3a544a89b64cc37009e940b344c0784487
VishakhaRapelli <125191036+VishakhaRapelli@users.noreply.github.com>
2023-03-30 08:02:46 +0530

    Add files via upload

--- TaxCalculator.java
+++ TaxCalculator.java
@@ -2,11 +2,28 @@ package com.cognizant.tax;
 
 public class TaxCalculator {
 	private float basicSalary;
-    private boolean citizenship;
+    private float tax;
+    private int nettSalary;
 
-    public TaxCalculator(float basicSalary, boolean citizenship) {
+
+
+    public TaxCalculator(float basicSalary) {
         this.basicSalary = basicSalary;
-        this.citizenship = citizenship;
+ 
+        this.tax = 0.0f;
+        this.nettSalary = 0;
+    }
+    public void calculateTax() {
+        this.tax = 30 * this.basicSalary / 100;
+        System.out.printf("The Tax of the employee for the %.2f is %.2f\n", this.basicSalary, this.tax);
+        
+    }
+    public void deductTax() {
+        this.nettSalary = (int)(this.basicSalary - this.tax);
+        System.out.printf("The nett salary of the employee is %d\n", this.nettSalary);
+        TaxCalculator employee = new TaxCalculator(50000);
+        employee.calculateTax();  
+        employee.deductTax(); 
     }
 
 
