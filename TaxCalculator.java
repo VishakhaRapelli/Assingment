f5de36e4144239f5a708bcdc6d5d782322b128b7
VishakhaRapelli <125191036+VishakhaRapelli@users.noreply.github.com>
2023-03-30 07:29:46 +0530

    Add files via upload

---
+++ TaxCalculator.java
@@ -0,0 +1,13 @@
+package com.cognizant.tax;
+
+public class TaxCalculator {
+	private float basicSalary;
+    private boolean citizenship;
+
+    public TaxCalculator(float basicSalary, boolean citizenship) {
+        this.basicSalary = basicSalary;
+        this.citizenship = citizenship;
+    }
+
+
+}
